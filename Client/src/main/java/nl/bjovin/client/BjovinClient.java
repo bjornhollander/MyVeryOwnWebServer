659165681d0c212499b1399b0ed25a6b3c161db9
Bjorn Hollander <bjornhollander@gmail.com>
2017-06-15 22:35:18 +0200

    Send message back and forth

--- Client/src/main/java/nl/bjovin/client/BjovinClient.java
+++ Client/src/main/java/nl/bjovin/client/BjovinClient.java
@@ -1,6 +1,13 @@
 package nl.bjovin.client;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,13 +19,33 @@ public class BjovinClient {
 	
 	public static void main(String[] args) {
 		try {
-			Socket socket = new Socket("localhost", PORT);
-			
+			Socket client = new Socket("localhost", PORT);
 			logger.info("Succesfully connected to server at port" + PORT + ".");
+			
+			PrintWriter outputWriter = getPrintWriter(client);
+			outputWriter.println("Hello from client!");
+			logger.info("Sending message...");
+			outputWriter.flush();
+			logger.info("Message send!");
+			
+			BufferedReader inputReader = getInputReader(client);
+			logger.info("Server wrote: " + inputReader.readLine());
+			
+			
 		} catch (IOException e) {
 			String message = "Unable to connect to server at port " + PORT + ".";
 			logger.log(Level.SEVERE, message, e);
 		}
+	}
+
+	private static BufferedReader getInputReader(Socket client) throws IOException {
+		InputStream inputStream = client.getInputStream();
+		return new BufferedReader(new InputStreamReader(inputStream));
+	}
+
+	private static PrintWriter getPrintWriter(Socket client) throws IOException {
+		OutputStream outputStream = client.getOutputStream();
+		return new PrintWriter(outputStream);
 	}
 	
 }
--- Server/src/main/java/nl/bjovin/server/core/BjovinServer.java
+++ Server/src/main/java/nl/bjovin/server/core/BjovinServer.java
@@ -1,6 +1,10 @@
 package nl.bjovin.server.core;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -13,17 +17,43 @@ public class BjovinServer {
 	
 	public static void main(String[] args) {
 		try {
-			ServerSocket server = new ServerSocket(PORT);
-			logger.info("Server launched. Listening at port " + PORT + ".");
+			Socket socket = launchServer();
 			
-			Socket socket = server.accept();
+			BufferedReader inputReader = getInputReader(socket);
+			logger.info("Client wrote: " + inputReader.readLine());
 			
-			logger.info("Client connected.");
+			PrintWriter writer = getWriter(socket);
+			writer.println("Hello from server!");
+			logger.info("Sending response...");
+			writer.flush();
+			
 		} catch (IOException e) {
 			String message = "Unable to open server socket at port " + PORT;
 			logger.log(Level.SEVERE, message, e);
 		}
 		
+		logger.info("Shutting down server.");
+	}
+
+	private static PrintWriter getWriter(Socket socket) throws IOException {
+		return new PrintWriter(socket.getOutputStream());
+	}
+
+	private static BufferedReader getInputReader(Socket socket) throws IOException {
+		InputStream socketInputStream = socket.getInputStream();
+		InputStreamReader reader = new InputStreamReader(socketInputStream);
+		return new BufferedReader(reader);
+	}
+
+	private static Socket launchServer() throws IOException {
+		ServerSocket server = new ServerSocket(PORT);
+		logger.info("Server launched. Listening at port " + PORT + ".");
+		
+		Socket socket = server.accept();
+		
+		logger.info("Client connected.");
+		
+		return socket;
 	}
 	
 }
